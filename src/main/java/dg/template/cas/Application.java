d0c5f96e1a1baf34f89d46a52e1b7d14a9d35b26
Musk.Chen <Musk.Chen@fanruan.com>
2022-07-12 20:21:22 +0800

    修改为Gradle

--- src/main/java/dg/template/cas/Application.java
+++ src/main/java/dg/template/cas/Application.java
@@ -2,8 +2,9 @@ package dg.template.cas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.apereo.cas")
 public class Application {
 
     public static void main(String[] args) {
